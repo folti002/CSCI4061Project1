0c07760fa33a8aa13c3435989361cf02a3da115e
Mikkel Folting <folti002@umn.edu>
2021-10-05 16:36:50 -0500

    Can now read degrees of each level from input file

--- src/master.c
+++ src/master.c
@@ -1,6 +1,9 @@
 #include "myutils.h"
 
 int main(int argc, char *argv[]) {
+    // ID of master process set
+    int myID = 0;
+    char* strID = "master";
 
     if (argc < 2) {
         printf("Less number of arguments.\n");
@@ -17,8 +20,8 @@ int main(int argc, char *argv[]) {
 
     // Parse input file
     FILE * fp;                                                      // File pointer
-    char *line = (char *)malloc(sizeof(char) * LineBufferSize);         // Line buffer where a new line is stored
-    size_t len = LineBufferSize;                                        // The size of the line buffer
+    char *line = (char *)malloc(sizeof(char) * LineBufferSize);     // Line buffer where a new line is stored
+    size_t len = LineBufferSize;                                    // The size of the line buffer
     ssize_t nread;                                                  // Length of a new line read
 
     char inputFileName[MaxFileNameLength];
@@ -37,6 +40,21 @@ int main(int argc, char *argv[]) {
     }
 
     // TODO: Read degrees of each level
+    int* degreesPerLevel = malloc(sizeof(int) * depth);
+    char temp;
+    int i = 0;
+    if((nread = getLineFromFile(fp, line, len)) != -1){
+        char* curChar = line;
+        while(*curChar != '\n'){
+            if(*curChar == ' '){
+                curChar++;
+            } else {
+                degreesPerLevel[i] = atoi(curChar);
+                i++;
+                curChar++;
+            }
+        }
+    }
 
     // Read input data
     int * input = (int *)malloc(sizeof(int) * nData);
@@ -51,11 +69,30 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
     // TODO: Spawn child processes and launch childProgram if necessary
+    pid_t pid;
+    pid_t terminated_pid;
+    if(depth == 0){
+
+    }
 
     // TODO: Wait all child processes to terminate if necessary
+    if(pid != 0){
+        terminated_pid = wait(NULL);
+    }
 
     // TODO: Merge sort or Quick sort (or other leaf node sorting algorithm)
+    
+    // myID as a string
+    char asStrID[MaxDepth];
+    // if(myID != 0){
+    //     char* args[] = {"childProgram", depth, myID, itoa(myID, strID, MaxDepth), 0, 0, 0, inputFileName};
+    //     execv("childProgram", args);
+    //     printf("Error executing childProgram\n");
+    //     exit(0);
+    //}
 
+    // CALL MERGE SORT
+    printf("%s Merge Sort\n", strID);
 
     free(input);
 
